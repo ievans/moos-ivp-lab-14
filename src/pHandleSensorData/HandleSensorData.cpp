30539eb8db7bb1210679775fc83915a1e9c7483f
Robert Truax <robtruax@mit.edu>
2012-04-04 11:54:11 -0400

    Uuo declaration and initial work.  WIP.

--- src/pHandleSensorData/HandleSensorData.cpp
+++ src/pHandleSensorData/HandleSensorData.cpp
@@ -39,6 +39,7 @@ bool HandleSensorData::OnNewMail(MOOSMSG_LIST &NewMail)
 
      string key = msg.GetKey();
 
+     // This is a classification report
      if (key == "UHZ_HAZARD_REPORT") {
        // Parse String
        string str = msg.GetString();
@@ -77,44 +78,11 @@ bool HandleSensorData::OnNewMail(MOOSMSG_LIST &NewMail)
 
        // check if we got everything necessary
        if (paramcount == 4) {
-	 map<int, Mine>::iterator it = _mines.find(label);
-	 if (it == _mines.end()) {
+	 // TODO:
+	 // INTEGRATE NEW DATA WITH BAYES
 
-	   Mine temp_mine;
-	   temp_mine.x = x;
-	   temp_mine.y = y;
-	   temp_mine.label = label;
-
-	   // Don't hold onto noisy first pass data
-	   if (_finished_survey) {
-	     if (hazard) {
-	       temp_mine.hazard_count = 1;
-	       temp_mine.neutral_count = 0;
-	     }
-	     else {
-	       temp_mine.hazard_count = 0;
-	       temp_mine.neutral_count = 1;
-	     }
-	   }
-	   else {
-	     temp_mine.hazard_count = 0;
-	     temp_mine.neutral_count = 0;
-	   }
-
-	   _mines.insert( pair<int,Mine>(label,temp_mine) );
-
-	 }
-	 // don't count noisy first pass survey data
-	 else if (_finished_survey) {
-	   if (hazard) {
-	     it->second.hazard_count++;
-	   }
-	   else {
-	     it->second.neutral_count++;
-	   }
-	 }
 	 m_Comms.Notify("LOCAL_SENSOR_MESSAGE", printStateMessage());
-	 cout << "Saw mine #" << label << endl;
+	 //	 cout << "Saw mine #" << label << endl;
 
 	 // Report to all vehicles the detection (when they come
 	 // within range
@@ -131,71 +99,68 @@ bool HandleSensorData::OnNewMail(MOOSMSG_LIST &NewMail)
        }
 
        // Do classification and print current state
-       //       classifyMines();
-       //              cout << printMineList() << endl;
+       //       classifyUuos();
+       //              cout << printUuoList() << endl;
 
      }
      else if (key == "HANDLE_SENSOR_MESSAGE") {
-       // Parse state of other vehicle and make copy
-       Mine mine;
-       map<int,Mine>::iterator it;
-       string instr = msg.GetString();
-       vector<string> strvect = parseString(instr,",");
-       for (int i = 0; i < strvect.size(); i++) {
-	 vector<string> in = parseString(strvect[i],":");
-	 if (in.size() != 5) {
-	   cout << "Invalid Mine format found at " << i << endl;
-	 }
-	 else {
-	   mine.label = atoi(in[0].c_str());
-	   mine.x = atof(in[1].c_str());
-	   mine.y = atof(in[2].c_str());
-	   mine.hazard_count = atoi(in[3].c_str());
-	   mine.neutral_count = atoi(in[4].c_str());
-
-	   it = _mines_other.find(mine.label);
-	   if (it == _mines_other.end()) {
-	     _mines_other.insert( pair<int,Mine>(mine.label,mine) );
-	   }
-	   else {
-	     it->second.hazard_count = mine.hazard_count;
-	     it->second.neutral_count = mine.neutral_count;
-	   }
-	 }
-       }
-     }
-
-     else if (key == "SURVEY") {
-       if (msg.GetString() == "COMPLETE") {
-	 _finished_survey = true;
-	 cout << "Survey Completed" << endl;
-       }
-       else {
-	 cout << "SURVEY not completed" << endl;
-       }
-     }
-
-     else if (key == "RECHECKED") {
-       if (msg.GetString() == "true") {
-	 _rechecked = true;
-	 cout << "Rechecked" << endl;
-       }
-       else {
-	 cout << "Rechecked message not true" << endl;
-       }
-     }
-
-     else if (key == "ALL_DONE") {
-       cout << "All_DONE received" << endl;
-       _allDone = true;
+       parseStateMessage(msg.GetString());
      }
    }
 	
    return(true);
 }
 
-void HandleSensorData::classifyMines() {
-  map<int, Mine>::iterator it, itother;
+void HandleSensorData::parseStateMessage(string msg) {
+  Uuo mine;
+  map<int,Uuo>::iterator it;
+  vector<string> strvect = parseString(msg,":");
+  for (int i = 0; i < strvect.size(); i++) {
+
+    Uuo newMine;
+    vector<string> in = parseString(strvect[i],",");
+    for (int j = 0; j < in.size(); j++) {
+      vector<string> var = parseString(in[j],"=");
+
+      if (var[0] == "id") {
+	newMine.id = atoi(var[1].c_str());
+      }
+      else if(var[0] == "x") {
+	newMine.x = atof(var[1].c_str());
+      }
+      else if(var[0] == "y") {
+	newMine.y = atof(var[1].c_str());
+      }
+      else if(var[0] == "pH") {
+	newMine.probHazard = atof(var[1].c_str());
+      }
+      else if(var[0] == "cc") {
+	newMine.classifyCount = atoi(var[1].c_str());
+      }
+    }
+
+    if (newMine.isInitialized()) {
+      // integrate new mine
+      it = _mines_other.find(newMine.id);
+      if (it == _mines_other.end()) {
+	_mines_other.insert( pair<int,Uuo>(newMine.id,newMine) );
+      }
+      else {
+	it->second = newMine;
+      }
+    }
+    else {
+      cout << "Invalid mine message received:" << endl;
+      cout << msg << endl;
+    }
+  }
+}
+
+void HandleSensorData::classifyUuos() {
+
+  //TODO
+  /*
+  map<int, Uuo>::iterator it, itother;
 
   // First, combine other_mine list into primary mine list
   for (itother = _mines_other.begin(); itother != _mines_other.end(); itother++) {
@@ -222,14 +187,16 @@ void HandleSensorData::classifyMines() {
       it->second.isHazard = false;
     }
   }
+  */
+  return;
 }
 
-string HandleSensorData::printMineList() {
+string HandleSensorData::printUuoList() {
   stringstream out;
-  map<int, Mine>::iterator it;
+  map<int, Uuo>::iterator it;
   for (it = _mines.begin(); it != _mines.end(); it++) {
-    out << "Mine[" << it->second.label <<"] = (" << it->second.x << ","
-	<< it->second.y << ") hazard=" << it->second.isHazard << endl;
+    out << "Uuo[" << it->second.id <<"] = (" << it->second.x << ","
+	<< it->second.y << ") hazard=" << it->second.isHazard() << endl;
   }
   return out.str();
 }
@@ -252,24 +219,29 @@ string HandleSensorData::printStateMessage() {
 
   // TODO:  Compress later
   // Message format:
-  // comma separated
-  // index:xpos:ypos:hazard-count:neutral-count
+  // : separated
+  // id,xpos,ypos,pH,cc
 
   stringstream msg;
-  map<int, Mine>::iterator it;
+  map<int, Uuo>::iterator it;
 
   if (_mines.begin() == _mines.end()) {
     msg << "NONE";
   }
   else {
     it = _mines.begin();
-    msg << it->second.label << ":" << it->second.x << ":" << it->second.y << ":"
-	<< it->second.hazard_count << ":" << it->second.neutral_count;
+    msg << "id=" << it->second.id << "," 
+	<< "x=" << it->second.x << "," 
+	<< "y=" << it->second.y << ","
+	<< "pH=" << it->second.probHazard << "," 
+	<< "cc=" << it->second.classifyCount;
 
     for (it++; it != _mines.end(); it++) {
-      msg << "," << it->second.label << ":" << it->second.x 
-	  << ":" << it->second.y << ":" << it->second.hazard_count 
-	  << ":" << it->second.neutral_count;
+      msg << ":" << "id=" << it->second.id << "," 
+	  << "x=" << it->second.x << "," 
+	  << "y=" << it->second.y << ","
+	  << "pH=" << it->second.probHazard << "," 
+	  << "cc=" << it->second.classifyCount;
     }
   }
 
@@ -282,13 +254,11 @@ string HandleSensorData::printStateMessage() {
 bool HandleSensorData::Iterate()
 {	
   //  cout << "Iterate Start" << endl;
-  // if (_finished_survey && _iter_count % 400 == 0) {
   if (_iter_count == 200) {
     // Constantly send state messages to other vehicle
     stringstream msg;
     msg << "src_node=" << tolower(_vehicle_name) 
 	<< ",dest_node=all,var_name=HANDLE_SENSOR_MESSAGE,string_val=\"" 
-      //     << _msg_idx << "  " << printStateMessage() << "\"";
 	<< printStateMessage() << "\"";
     m_Comms.Notify("NODE_MESSAGE_LOCAL", msg.str() );
     _msg_idx++;
@@ -296,25 +266,14 @@ bool HandleSensorData::Iterate()
 
     // Post local copy
     m_Comms.Notify("LOCAL_SENSOR_MESSAGE", printStateMessage());
+
+    // reset iteration counter
+    _iter_count = 0;
   }
-  else if (_iter_count == 400) {
-    if (_rechecked) {
-      stringstream out;
-      out << "src_node=" << tolower(_vehicle_name) 
-	  << ",dest_node=all,var_name=ALL_DONE,string_val=true";
-      m_Comms.Notify("NODE_MESSAGE_LOCAL", out.str());
-    }
-  }
-  else if (_iter_count == 401) {
-    if (_isPrimary && _rechecked && _allDone && !_lockout) {
-      if (_report_count > 1) {
-	// send final message 
-	classifyMines();
-	generateHazardReport();
-	_lockout = true;
-      }
-      _report_count++;
-    }
+  else if (_isPrimary && !_lockout && MOOSTime() - _starttime > _endtime) {
+    classifyUuos();
+    generateHazardReport();
+    _lockout = true;
     _iter_count = 0;
   }
 
@@ -324,18 +283,18 @@ bool HandleSensorData::Iterate()
 
 void HandleSensorData::generateHazardReport() {
   // Generate and publish a hazard report
-  map<int, Mine>::iterator it;
+  map<int, Uuo>::iterator it;
   XYHazardSet set;
   set.setSource(tolower(_vehicle_name));
   for (it = _mines.begin(); it != _mines.end(); it++) {
     string type;
-    if (it->second.isHazard) {
+    if (it->second.isHazard()) {
       type = "hazard";
     }
     else {
       type = "benign";
     }
-    set.addHazard(it->second.x, it->second.y, type, intToString(it->second.label));
+    set.addHazard(it->second.x, it->second.y, type, intToString(it->second.id));
   }
 
   string out = set.getSpec();
@@ -355,11 +314,12 @@ bool HandleSensorData::OnStartUp()
   _isPrimary = false;
   _msg_idx = 0;
   _iter_count = 0;
-  _finished_survey = false;
-  _rechecked = false;
+  //  _finished_survey = false;
+  //  _rechecked = false;
   _lockout = false;
-  _allDone = false;
+  //  _allDone = false;
   _report_count = 0;
+  _starttime = MOOSTime();
 
   STRING_LIST sParams;
   m_MissionReader.GetConfiguration(GetAppName(), sParams);
@@ -410,9 +370,10 @@ void HandleSensorData::RegisterVariables()
 {
   //m_Comms.Register("FOOBAR", 0);
   m_Comms.Register("UHZ_HAZARD_REPORT", 0);
-  m_Comms.Register("SURVEY", 0);
-  m_Comms.Register("RECHECKED", 0);
-  m_Comms.Register("ALL_DONE", 0);
+  m_Comms.Register("UHZ_DETECTION_REPORT",0);
+  //  m_Comms.Register("SURVEY", 0);
+  //  m_Comms.Register("RECHECKED", 0);
+  //  m_Comms.Register("ALL_DONE", 0);
 
   if (_isPrimary) {
     m_Comms.Register("HANDLE_SENSOR_MESSAGE", 0);
--- src/pHandleSensorData/HandleSensorData.h
+++ src/pHandleSensorData/HandleSensorData.h
@@ -15,14 +15,44 @@
 
 using namespace std;
 
-class Mine {
+class Uuo { // unitenditfied underwater vehicle
  public:
   double x;
   double y;
-  int hazard_count;
-  int neutral_count;
-  int label;
-  bool isHazard;
+  int id;
+  double probHazard;
+  int classifyCount; // remaining times to classify object
+  // Todo: raw sightings from vehicles
+
+  Uuo () {
+    // Requires initialization
+    x = -1000;
+    y = -1000;
+    id = -1;
+
+    // default initialization
+    probHazard = 0.5; // Maximum entropy?  Better prior?
+    classifyCount = 0;
+  }
+
+  bool isHazard() {
+    // Todo: Be smarter!
+    if (probHazard > 0.75) {
+      return true;
+    }
+    else {
+      return false;
+    }
+  }
+
+  bool isInitialized() {
+    if (x == -1000 || y == -1000 || id == -1) {
+      return false;
+    }
+    else {
+      return true;
+    }
+  }
 };
 
 class HandleSensorData : public CMOOSApp
@@ -40,22 +70,25 @@ class HandleSensorData : public CMOOSApp
 
 private:
 
-   string printMineList();
+   string printUuoList();
    string printStateMessage();
-   void classifyMines();
+   void classifyUuos();
    void generateHazardReport();
+   void parseStateMessage(string);
 
    string _vehicle_name;
-   map<int, Mine> _mines;
-   map<int, Mine> _mines_other;
+   map<int, Uuo> _mines;
+   map<int, Uuo> _mines_other;
    bool _isPrimary;
    int _msg_idx;
    int _iter_count;
-   bool _finished_survey;
-   bool _rechecked;
+   //  bool _finished_survey;
+   //  bool _rechecked;
    bool _lockout;
-   bool _allDone;
+   //  bool _allDone;
    int _report_count;
+   double _starttime;
+   double _endtime;
 };
 
 #endif 
